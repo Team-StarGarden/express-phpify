0602abbf68474f73591ec715c1ee96aae65bbf8b
Alex4386 <psh010209@gmail.com>
2018-12-24 19:46:35 +0900

    1.1.0 Update By RanolP

--- lib/index.ts
+++ lib/index.ts
@@ -14,25 +14,42 @@ function createFakePHPSESSID(): string {
   return text;
 }
 
-export function phpify(app: Application) {
+export interface PhpifyOption {
+  headerXPoweredBy?: string;
+  fakePHPSESSID?: boolean;
+  forceRedirectToPHPExtension?: boolean;
+  allowDirectory?: boolean;
+}
+
+export function phpify(app: Application, option?: PhpifyOption) {
   app.use(cookieParser());
 
   app.use((req, res, next) => {
-    res.setHeader('X-Powered-By', 'PHP 5.3.3');
-    const cookie = req.cookies.cookieName;
-    if (!cookie) {
-      res.cookie('PHPSESSID', createFakePHPSESSID());
-    } else if (!cookie.PHPSESSID) {
-      res.cookie('PHPSESSID', createFakePHPSESSID());
+    if (option.headerXPoweredBy) {
+      res.setHeader('X-Powered-By', option.headerXPoweredBy || 'PHP 5.3.3');
     }
-    if (!req.url.toLowerCase().endsWith('.php') && !req.url.toLowerCase().endsWith('/')) {
-        res.redirect(req.url+'.php');
+    if (option.fakePHPSESSID !== false) {
+      const cookie = req.cookies.cookieName;
+      if (!cookie || !cookie.PHPSESSID) {
+        res.cookie('PHPSESSID', createFakePHPSESSID());
+      }
+    }
+
+    if (
+      !req.url.toLowerCase().endsWith('.php') &&
+      (!req.url.toLowerCase().endsWith('/') &&
+        option.allowDirectory !== false) &&
+      option.forceRedirectToPHPExtension !== false
+    ) {
+      res.redirect(req.url + '.php');
     }
     if (/=PHP(.+)/.test(req.url.split('?')[1])) {
-        if (req.url.split('?')[1] === "=PHPE9568F36-D428-11d2-A769-00AA001ACF42") {
-            console.log("Easteregg Triggered");
-        }
-        // More EasterEggs Later
+      if (
+        req.url.split('?')[1] === '=PHPE9568F36-D428-11d2-A769-00AA001ACF42'
+      ) {
+        console.log('Easteregg Triggered');
+      }
+      // More EasterEggs Later
     }
     next();
   });
