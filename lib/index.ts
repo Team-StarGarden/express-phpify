d8b6d960d89227056c47e04240e5c772247a4fa1
Alex4386 <psh010209@gmail.com>
2018-12-24 20:35:44 +0900

    Wow, v1.1.0 HUGE CONFIGURATION UPDATE SUCH UPDATE, WOW

--- lib/index.ts
+++ lib/index.ts
@@ -14,39 +14,115 @@ function createFakePHPSESSID(): string {
   return text;
 }
 
+/**
+ * The interface which contains the option of phpifying.
+ */
 export interface PhpifyOption {
-  headerXPoweredBy?: string;
+  /**
+   * Header Field `X-Powered-By`.
+   */
+  headerXPoweredBy?: false | string;
+  /**
+   * Whether to disable fake PHPSESSID.
+   */
   fakePHPSESSID?: boolean;
-  forceRedirectToPHPExtension?: boolean;
-  allowDirectory?: boolean;
+  /**
+   * Redirect to php extension option.
+   */
+  redirection?: false | PhpifyRedirectionOption;
 }
 
+/**
+ * The interface which contains the redirection option of phpifying.
+ */
+export interface PhpifyRedirectionOption {
+  /**
+   * Whether to disable force redirecting to php extension if the user connected to file other extension (like .html, .jsp).
+   */
+  file?: boolean;
+  /**
+   * Whether to force redirecting to php extension if the user connected to folder.
+   */
+  directory?: false | DirectoryRedirectionOption;
+}
+
+/**
+ * The interface which contains the directory redirection option of phpifying.
+ */
+export interface DirectoryRedirectionOption {
+  /**
+   * The file name of index.
+   */
+  indexFileName?: string;
+}
+
+const easterEggRegex = /=PHP(.+)/;
+
+/**
+ * Phpify your app with the option.
+ * @param app the express application
+ * @param option the phpify option
+ */
 export function phpify(app: Application, option?: PhpifyOption) {
   app.use(cookieParser());
 
   app.use((req, res, next) => {
-    if (option.headerXPoweredBy) {
+    if (option.headerXPoweredBy !== false) {
       res.setHeader('X-Powered-By', option.headerXPoweredBy || 'PHP 5.3.3');
     }
     if (option.fakePHPSESSID !== false) {
       const cookie = req.cookies.cookieName;
-      if (!cookie || !cookie.PHPSESSID) {
+      if (!cookie) {
+        res.cookie('PHPSESSID', createFakePHPSESSID());
+      }
+      if (!cookie.PHPSESSID) {
         res.cookie('PHPSESSID', createFakePHPSESSID());
       }
     }
 
-    if (
-      !req.url.toLowerCase().endsWith('.php') &&
-      (!req.url.toLowerCase().endsWith('/') &&
-        option.allowDirectory !== false) &&
-      option.forceRedirectToPHPExtension !== false
-    ) {
-      res.redirect(req.url + '.php');
+    if (option.redirection !== false) {
+      const dot = req.url.lastIndexOf('.');
+      const routeWithoutExtension = req.url.substring(
+        0,
+        dot < 0 ? req.url.length : dot
+      );
+      const extension = req.url.substring(dot < 0 ? 0 : dot + 1);
+      const isPHP: boolean = extension === 'php';
+      const isDir: boolean = req.url.endsWith('/');
+
+      if (option.redirection === null) {
+        // Use default value (true for all).
+        if (!isPHP) {
+          if (isDir) {
+            res.redirect(req.url + 'index.php');
+          } else {
+            res.redirect(routeWithoutExtension + '.php');
+          }
+        }
+      } else {
+        if (option.redirection.file !== false && !isDir && !isPHP) {
+          res.redirect(routeWithoutExtension + '.php');
+        }
+        if (option.redirection.directory !== false && isDir) {
+          if (option.redirection.directory === null) {
+            // Use default value (true).
+            res.redirect(
+              req.url + 'index.php'
+            );
+          } else {
+            res.redirect(
+              req.url +
+                (option.redirection.directory.indexFileName || 'index.php')
+            );
+          }
+        }
+      }
     }
-    if (/=PHP(.+)/.test(req.url.split('?')[1])) {
-      if (
-        req.url.split('?')[1] === '=PHPE9568F36-D428-11d2-A769-00AA001ACF42'
-      ) {
+
+    const data = req.url.split('?')[1];    
+
+    if (easterEggRegex.test(data)) {
+      if (data === '=PHPE9568F36-D428-11d2-A769-00AA001ACF42') {
         console.log('Easteregg Triggered');
       }
       // More EasterEggs Later
@@ -54,3 +130,15 @@ export function phpify(app: Application, option?: PhpifyOption) {
     next();
   });
 }
+
+import express from 'express';
+
+const app = express();
+phpify(app, {
+  headerXPoweredBy: 'PHP 5.3.3',
+  fakePHPSESSID: true,
+  redirection: {
+    file: true,
+    directory: false
+  }
+})
